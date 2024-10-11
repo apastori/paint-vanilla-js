67c476435d7f12e60fb83c4a09c77a358d61ee1d
apastori <alfonso.pastori@ibm.com>
2024-10-11 20:47:25 -0300

    DrawingCanvasBasic

--- script.js
+++ script.js
@@ -1,7 +1,62 @@
+const Modes = {
+    DRAW: "draw",
+    ERASE: "erase",
+    RECTANGLE: "rectangle",
+    ELLIPSE:"ellipse",
+
+}
+
 const selector = (selector) => {
     return document.querySelector(selector);
 }
 
 const selectorList = (selector) => {
     return document.querySelectorAll(selector);
+}
+
+const canvas = selector("#canvas-paint");
+
+const context = canvas.getContext("2d");
+
+let startX, startY;
+let lastX, lastY;
+let isDrawing = false;
+
+canvas.addEventListener("mousedown", (event) => {
+    startDrawing(event);
+});
+
+canvas.addEventListener("mousemove", (event) => {
+    draw(event)
+});
+
+canvas.addEventListener("mouseup", (event) => {
+    stopDrawing(event);
+});
+
+canvas.addEventListener("mouseleave", (event) => {
+    stopDrawing(event);
+});
+
+function startDrawing(event) {
+    isDrawing = true;
+    const { offsetX, offsetY } = event;
+    [startX, startY] = [offsetX, offsetY];
+    [lastX, lastY] = [offsetX, offsetY];
+}
+
+function stopDrawing(event) {
+    isDrawing = false;
+}
+
+function draw(event) {
+    if (!isDrawing) return;
+    const { offsetX, offsetY } = event;
+    //start drawing
+    context.beginPath();
+    //move to current coordinates
+    context.moveTo(lastX, lastY);
+    context.lineTo(offsetX, offsetY);
+    context.stroke();
+    [lastX, lastY] = [offsetX, offsetY];
 }
